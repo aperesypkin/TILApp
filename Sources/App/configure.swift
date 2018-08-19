4e4dca8f1f1c0e0ea3770300e2d4fc7a2d7415f9
Alexander Peresypkin <peresypkin10@gmail.com>
2018-08-19 21:25:26 +0300

    Bug fix

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -45,7 +45,7 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     
     /// Configure migrations
     var migrations = MigrationConfig()
-    migrations.add(migration: Acronym.self, database: .psql)
+    migrations.add(model: Acronym.self, database: .psql)
     services.register(migrations)
 
 }
