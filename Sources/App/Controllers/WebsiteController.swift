6fac3120fffe44ba1b3cb43c5eb51cd2249076b9
Alexander Peresypkin <peresypkin10@gmail.com>
2018-09-08 17:09:00 +0300

    Add twitterURL to User and make Categories unique

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -188,7 +188,11 @@ class WebsiteController: RouteCollection {
             return req.future(req.redirect(to: redirect))
         }
         let password = try BCrypt.hash(data.password)
-        let user = User(name: data.name, username: data.username, password: password)
+        var twitterURL: String?
+        if let twitter = data.twitterURL, !twitter.isEmpty {
+            twitterURL = twitter
+        }
+        let user = User(name: data.name, username: data.username, password: password, twitterURL: twitterURL)
         return user.save(on: req).map(to: Response.self) { user in
             try req.authenticateSession(user)
             return req.redirect(to: "/")
@@ -279,6 +283,7 @@ struct RegisterData: Content {
     let username: String
     let password: String
     let confirmPassword: String
+    let twitterURL: String?
 }
 
 extension RegisterData: Validatable, Reflectable {
---
+++ Sources/App/Migrations/08-09-18-MakeCategoriesUnique.swift
@@ -0,0 +1,18 @@
+import Vapor
+import FluentPostgreSQL
+
+struct MakeCategoriesUnique: Migration {
+    typealias Database = PostgreSQLDatabase
+    
+    static func prepare(on conn: PostgreSQLConnection) -> Future<Void> {
+        return Database.update(Category.self, on: conn) { builder in
+            builder.unique(on: \.name)
+        }
+    }
+    
+    static func revert(on conn: PostgreSQLConnection) -> Future<Void> {
+        return Database.update(Category.self, on: conn) { builder in
+            builder.deleteUnique(from: \.name)
+        }
+    }
+}
